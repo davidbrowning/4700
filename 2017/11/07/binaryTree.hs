a9a98b319225fa574761bc23f9dc3a6a7df833e0
davidbrowning <dmbrowning21@gmail.com>
2017-11-08 18:33:32 -0700

    working binary tree

--- 2017/11/07/binaryTree.hs
+++ 2017/11/07/binaryTree.hs
@@ -1,17 +1,18 @@
 data Color = Red | Black deriving Show
 data Tree x = Empty | Node Color (Tree x) x (Tree x) deriving Show
 
-insert Empty value = Node Empty val Empty
+insert Empty val = Node Red Empty val Empty
 insert (Node c left val right) x 
  | x == val = Node c left val right
- | x < val Node c (balance(insert left x)) val right
- | x > val Node c left val (balance (insert right x))
+ | x < val = balance(Node c (insert left x) val right)
+ | x > val = balance(Node c left val (insert right x))
 
 makeBlack (Node Red l v r) = Node Black l v r
 makeBlack x = x
-treeInsert t v = makeBlack.insert t v 
+treeInsert t v = makeBlack (insert t v)
 
-balance (Node Black (Node Red a x (Node Red b y c)) z d = Node Red (Node Black a x b) y (Node Black c z d)
+balance (Node Black (Node Red a x (Node Red b y c)) z d) = Node Red (Node Black a x b) y (Node Black c z d)
 balance t = t
 
 -- from here use foldl insert Empty [5,1,4,4,2,7,3,8]
+--(balance)
