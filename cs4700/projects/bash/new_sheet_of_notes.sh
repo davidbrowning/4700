7ae51a9482e0887bfd8df966160501bf5dc67f8e
davidbrowning <dmbrowning21@gmail.com>
2017-12-02 09:31:11 -0700

    Fine tuning

--- cs4700/projects/bash/new_sheet_of_notes.sh
+++ cs4700/projects/bash/new_sheet_of_notes.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+
+#######################################################################################
+#
+#  When you don't have time to set up your notes and just need to start writing.
+#  Two test cases exist:
+#   1) modify find_out_which_class as instructed below to run test case #1
+#   2) uncomment line 98 for test case #2
+#
+#######################################################################################
+
 schedule_file="schedule.sh"
-if [ -z $(ls $schedule_file) ]; then
+if [ ! -f $schedule_file ]; then
   read -p "No schedule file found, would you like to make one? y/n: " ans
   if [ $ans == 'y' ]; then
     printf "declare -A schedule\n" >> $schedule_file
@@ -31,17 +41,18 @@ fi
 source schedule.sh
 
 function find_out_which_class () {
-  #for the sake of testing, it is 10:45 on a Tuesday
-  day_schedge_testing=${schedule[Tuesday]};
-  current_hour_testing=10
-  current_minute_testing=45
-  day_schedge=${schedule[$(date +%A)]};
-  current_hour=$(date +%H);
-  current_minute=$(date +%M);
-  number_of_classes=$(echo $day_schedge_testing | grep -o ',' | wc -l);
+  # test case assumes you have the schedule.sh included in my repo
+  # to run a test case, uncomment the following 3 lines, and comment out their corresponding ones.
+  # day_schedge=${schedule[Tuesday]};
+  # current_hour=10
+  # current_minute=45
+  day_schedge=${schedule[$(date +%A)]}; # 1
+  current_hour=$(date +%H); # 2
+  current_minute=$(date +%M); #3
+  number_of_classes=$(echo $day_schedge | grep -o ',' | wc -l);
   current_course="";
   for ((i = 1; i <= number_of_classes; i += 1)); do
-    course_info=$(echo $day_schedge_testing | cut -d ',' -f$i);
+    course_info=$(echo $day_schedge | cut -d ',' -f$i);
     course_name=$(echo $course_info | cut -d ';' -f1);
     time=$(echo $course_info | cut -d ';' -f2);
     start_time=$(echo $time | cut -d '-' -f1);
@@ -50,9 +61,9 @@ function find_out_which_class () {
     course_start_minute=$(echo $start_time | cut -d ':' -f2);
     course_end_hour=$(echo $end_time | cut -d ':' -f1);
     course_end_minute=$(echo $end_time | cut -d ':' -f2);
-    if ([ $current_hour_testing -ge $course_start_hour ] && [ $current_minute_testing -ge $course_start_minute ]);
+    if ([ $current_hour -ge $course_start_hour ] && [ $current_minute -ge $course_start_minute ]);
     then
-      if ([ $current_hour_testing -le $course_end_hour ] && [ $current_minute_testing -le $course_end_minute ]);
+      if ([ $current_hour -le $course_end_hour ] && [ $current_minute -le $course_end_minute ]);
       then
         # you are currently in the time frame of the given course
         current_course=$course_name
@@ -83,32 +94,38 @@ fi
 
 cd $d
 
-if [ ! -f notes.tex ]; then
-  >notes.tex
-  echo "\\title{$current_course}" >> notes.tex
-  echo "\\author{$USER}" >> notes.tex
-  echo "\\date{\\today}" >> notes.tex
-  echo "\\documentclass[12pt]{article}" >> notes.tex
-  echo "\\begin{document}" >> notes.tex
-  echo "\\maketitle" >> notes.tex
-  echo "\\section{Topics To Be Covered}" >> notes.tex
-  echo "\\end{document}" >> notes.tex
+# Note: If you wish to run a test case, uncomment the following line 
+# echo "preforming test case!"; current_course="Programming Languages" 
+
+notes_name=$(echo $current_course | cut -d ' ' -f1)
+notes_name=$notes_name$(echo $current_course | cut -d ' ' -f2).tex
+echo $notes_name
+if [ ! -f $notes_name ]; then
+  >$notes_name
+  echo "\\title{$current_course}" >> $notes_name
+  echo "\\author{$USER}" >> $notes_name
+  echo "\\date{\\today}" >> $notes_name
+  echo "\\documentclass[12pt]{article}" >> $notes_name
+  echo "\\begin{document}" >> $notes_name
+  echo "\\maketitle" >> $notes_name
+  echo "\\section{Topics To Be Covered}" >> $notes_name
+  echo "\\end{document}" >> $notes_name
 else
-  read -p "notes.tex already exists, do you want to overwrite? y/n" a
+  read -p "$notes_name already exists, do you want to overwrite? y/n: " a
   if [ $a == 'y' ]; then
-    > notes.tex
-    echo "\\title{$current_course}" >> notes.tex
-    echo "\\author{$USER}" >> notes.tex
-    echo "\\date{\\today}" >> notes.tex
-    echo "\\documentclass[12pt]{article}" >> notes.tex
-    echo "\\begin{document}" >> notes.tex
-    echo "\\maketitle" >> notes.tex
-    echo "\\section{Topics To Be Covered}" >> notes.tex
-    echo "\\end{document}" >> notes.tex
+    > $notes_name
+    echo "\\title{$current_course}" >> $notes_name
+    echo "\\author{$USER}" >> $notes_name
+    echo "\\date{\\today}" >> $notes_name
+    echo "\\documentclass[12pt]{article}" >> $notes_name
+    echo "\\begin{document}" >> $notes_name
+    echo "\\maketitle" >> $notes_name
+    echo "\\section{Topics To Be Covered}" >> $notes_name
+    echo "\\end{document}" >> $notes_name
   fi
 fi
-vim notes.tex +7
-pdflatex notes.tex
+vim $notes_name +7
+pdflatex $notes_name
 echo "Path to your notes: $(pwd)"
 
 exit
